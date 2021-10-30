6a94bf3a1afbb979e344259a0fe5b372d831611a
Aleksandr Daniel <my_mail@example.com>
2021-10-30 16:30:20 +0300

    Импортировал функции для перевода форм в активное, и не активное состояние

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import { createAdvertisements } from './mock/create-advertisements.js';
 import { NUMBEROBJECTGENERATE } from './mock/data.js';
 import { showPopup } from './popup.js';
-import { switchToDisabled } from './form-disabled.js';
+import { switchToDisabled, switchToAvalabled } from './form.js';
+
 
 const showData = createAdvertisements(NUMBEROBJECTGENERATE);
 showPopup(showData[0]);
 switchToDisabled();
+switchToAvalabled();
