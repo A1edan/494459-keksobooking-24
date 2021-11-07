1d67ba4d10f45d51ae832568069c224bdf824353
Aleksandr Daniel <my_mail@example.com>
2021-11-07 19:18:17 +0200

    Реализовал проверку форм "Тип жилья" и "Количество комнат", и поправил некоторые ошибки.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import { createAdvertisements } from './mock/create-advertisements.js';
 import { NUMBEROBJECTGENERATE } from './mock/data.js';
 import { showPopup } from './popup.js';
-import { switchToDisabled, switchToAvalabled } from './form.js';
+import { switchToDisabled, switchToAvalabled, enableFilter, enableForm } from './form.js';
 
 
 const showData = createAdvertisements(NUMBEROBJECTGENERATE);
 showPopup(showData[0]);
 switchToDisabled();
 switchToAvalabled();
+enableFilter();
+// enableForm();
