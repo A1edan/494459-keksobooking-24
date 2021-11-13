7e63624df71a343e00eecc81e29827653e61293e
Aleksandr Daniel <my_mail@example.com>
2021-11-13 16:36:50 +0200

    Восстановил удалённые импорты и вызываемые функции

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-import { formDisabled, addFormHandlers } from './form.js';
+import { createAdvertisements } from './mock/create-advertisements.js';
+import { COUNT_ADS } from './mock/data.js';
+import { showPopup } from './popup.js';
+import { formDisabled, formEnabled, addFormHandlers } from './form.js';
 import { addMap } from './map.js';
 
+
+const adsData = createAdvertisements(COUNT_ADS);
+showPopup(adsData[0]);
 formDisabled();
+formEnabled();
 addFormHandlers();
 addMap();
